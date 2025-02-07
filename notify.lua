3ebdd6b528bb16a97c4af43581f57ae5f5a0e810
perfectusmim1 <123786459+perfectusmim1@users.noreply.github.com>
2025-02-07 22:02:18 +0300

    Update notify.lua

--- notify.lua
+++ notify.lua
@@ -1,3 +1,6 @@
+
+local Rayfield = Rayfield or loadstring(game:HttpGet("https://raw.githubusercontent.com/perfectusmim1/script/refs/heads/main/editedui", true))()
+
 local success, errorMessage = pcall(function()
     local unctestCode = loadstring(game:HttpGet("https://raw.githubusercontent.com/perfectusmim1/yedek/refs/heads/main/uncTest.lua", true))
     if unctestCode then
@@ -22,7 +25,7 @@ task.defer(function()
     repeat task.wait() until _G.uncResult ~= nil
     Rayfield:Notify({
         Title = "UNC Test Result",
-        Content = _G.uncResult, 
+        Content = _G.uncResult,
         Duration = 5,
         Image = 133212968396061,
     })
